e9f2c4b2ab1aa868451e415758325918a3b85b6d
Daniel Barrett <dbarrett@ezesoft.com>
2016-02-08 12:48:39 -0500

    cleanup, add images dir to gitignor

--- satellites.js
+++ satellites.js
@@ -8,23 +8,25 @@ var http    = require('http'),
 
 var SatellitePrototype = {
     constructor: function (name, fetch) {
-        this.name = name;
+        this._name = name;
         this.fetch = fetch;
-        this.latest = new Date(0);
-        this.imagepath = __dirname + path.sep + 'images' + path.sep + name
-        mkdirp(this.imagepath, function(err) {
+        this._latest = new Date(0);
+        this._imagepath = __dirname + path.sep + 'images' + path.sep + name
+        mkdirp(this._imagepath, function(err) {
             if(err) {
                 console.log('Error: could not create image path for: ' + name)
                 process.exit(1);   
             }
         });
-    }
+    },
+    name : function() { return this._name },
+    latest : function() { return this._latest },
+    imagepath : function() { return this._imagepath },
+    updateLatest : function() { this._latest = Date.now() }
 };
 
 var discover = Object.create(SatellitePrototype);
 discover.constructor('dscovr', function(callback) {
-    console.log("fetching discover...");
-    console.log("this: " + JSON.stringify(this));
     http.get({
         host: 'epic.gsfc.nasa.gov',
         path: '/api/images.php'
@@ -35,20 +37,28 @@ discover.constructor('dscovr', function(callback) {
         });
         response.on('end', function() {
             var records = JSON.parse(body);
-            console.log("this: " + this);
-            console.log(records[records.length - 1].date)
-            console.log(this.latest)
-            if(records && (this.latest < Date.parse(records[records.length - 1].date))) {
+            if(records && (SatellitePrototype.latest.call(discover) < Date.parse(records[records.length - 1].date))) {
                 var imagename = records[records.length - 1].image + '.jpg'
-                console.log('found a more recent image. downloading...')
                 var uri = 'http://epic.gsfc.nasa.gov/epic-archive/jpg/' + imagename;
-                var filename = this.path + path.sep + imagename
+                var filename = SatellitePrototype.imagepath.call(discover) + path.sep + imagename
                 request.head(uri, function(err, res, img) {
                     if(err) { callback(err) }
                     console.log('content-type:', res.headers['content-type']);
                     console.log('content-length:', res.headers['content-length']);
+                    console.log('writing to: ', filename);
                     
-                    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback(null, filename));
+                    var r = request(uri).pipe(fs.createWriteStream(filename));
+                    r.on('error', function() { callback('error with download') })
+                    r.on('close', function() {
+                        fs.stat(filename, function(err, stat) {
+                            if(stat.size > 0) {
+                                SatellitePrototype.updateLatest.call(discover);
+                                callback(null, filename)
+                            } else {
+                                callback('filesize is 0')
+                            }
+                        });
+                    });
                 })
             } else {
                 callback("already have the latest")
